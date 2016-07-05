722e186849c3337eb7eaabe08377f63e2f4ab591
Chris Picato <christopherpicato@gmail.com>
2016-07-05 18:52:52 -0400

    Finishes recursion assignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  // var results = [];
+  // var all = document.getElementsByTagName('*');
+  // for (var i = 0; i < all.length; i++) {
+  //   if ($(all[i]).hasClass(className)) {
+  //     results.push(all[i]);
+  //   }
+  // }
+  // return results;
+  var results = [];
+  var checkElement = function (element, className) {
+    if (element.nodeName !== '#text') {
+      if (element.classList.contains(className)) {
+        results.push(element);
+      }
+      for (var i = 0; i < element.childNodes.length; i++) {
+        checkElement(element.childNodes[i], className);
+      }
+    }
+  };
+  checkElement(document.body, className);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return String(obj);
+  }
+  if (obj === undefined || obj.constructor === Function) {
+    return;
+  }
+  if (obj.constructor !== Object && obj.constructor !== Array) {
+    if (obj.constructor === String) {
+      return "\"" + obj + "\""; // concatenates obj with parens
+    }
+    if (obj.constructor === Number) {
+      return obj.toString();
+    }
+    return String(obj);
+  }
+  if (obj.constructor === Array) {
+    var newStringArray = new Array();
+    for (var i = 0; i < obj.length; i++) {
+      newStringArray.push(stringifyJSON(obj[i]));
+    }
+    return "[" + newStringArray.join(",") + "]";
+  }
+  if (obj.constructor === Object) {
+    var newStringArray = new Array();
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+        newStringArray.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+      }
+    }
+    return "{" + newStringArray.join(",") + "}";
+  }
 };
